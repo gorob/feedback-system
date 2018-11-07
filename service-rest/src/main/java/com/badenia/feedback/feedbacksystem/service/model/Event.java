f50fa71eb7d4e5566955c617b63a4760c4d36817
Constantin Krüger <Constantinidis@web.de>
2018-11-07 22:30:37 +0100

    fix compilation error, because of missed to checkin

--- service-rest/src/main/java/com/badenia/feedback/feedbacksystem/service/model/Event.java
+++ service-rest/src/main/java/com/badenia/feedback/feedbacksystem/service/model/Event.java
@@ -3,10 +3,12 @@ package com.badenia.feedback.feedbacksystem.service.model;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class Event {
 
 	private Long id;
