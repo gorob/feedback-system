471aefc13f27db07ab052295f56acba83ec093c0
andreisava <andreisava@192.168.0.88>
2018-11-10 19:14:03 +0100

    add Anno to EventController
    @CrossOrigin(origins="*")
    
    Damit Security zwischen den Frontend und Backend ausgeschaltet ist.
    Sonst wird der Backend den Request ablehnen

--- service-rest/src/main/java/com/badenia/feedback/feedbacksystem/controller/EventController.java
+++ service-rest/src/main/java/com/badenia/feedback/feedbacksystem/controller/EventController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +17,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("/v1/feedback/events")
 @Getter(AccessLevel.PROTECTED)
 public class EventController {
 
 	@Autowired
 	private IFeedbackService feedbackService;
-
+	
 	@GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<List<Event>> getEvents() {
 		List<Event> events = getFeedbackService().findEvents();
