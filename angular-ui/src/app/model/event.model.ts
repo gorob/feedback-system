e96eb5d5b4ccb59a67d03c2f182b7108e0de9e9a
andreisava <andreisava@192.168.0.88>
2018-11-11 10:19:41 +0100

    delete prj angular-ui
    delete weil die CLI nature nicht mit kopiert wurde

--- angular-ui/src/app/model/event.model.ts
+++
@@ -1,8 +0,0 @@
-
-import { Question } from './question.model';
-
-export class Event {
-  id: number;
-  name: string;
-  questions: Question;
-}
--- angular-ui/src/app/model/question.model.ts
+++
@@ -1,5 +0,0 @@
-export class Question {
-  id: number;
-  questionName: string;
-
-}
