fe4c2fc492d1d92f6a16a4df361f53a244b65717
andreisava <andreisava@192.168.0.88>
2018-11-28 22:09:32 +0100

    created postMethod from view

--- thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/controller/EventsController.java
+++ thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/controller/EventsController.java
@@ -1,26 +1,59 @@
 package com.badenia.feedback.thymeleaf.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.badenia.feedback.thymeleaf.model.Event;
 import com.badenia.feedback.thymeleaf.model.Question;
 import com.badenia.feedback.thymeleaf.service.FeedbackClientService;
 import com.badenia.feedback.thymeleaf.service.IFeedbackClientService;
 
+import lombok.Getter;
+
+@Getter
 @Controller
 public class EventsController {
 	
 	IFeedbackClientService serviceReposiroty = new FeedbackClientService();
+	private List<Event> allEvents;
+	
+	public EventsController() {
+		this.allEvents = serviceReposiroty.leseAlleEvents();
+	}
+	
+	@ModelAttribute(value="event")
+	public Event newEvent() {
+		return new Event();
+	}
 	
 	@GetMapping("/events")
-	public String index(Model model) {
-		List<Event> allEvents = serviceReposiroty.leseAlleEvents();
-		model.addAttribute("events", allEvents);
-		return "events";
+	public ModelAndView index(Model model, HttpServletRequest request) {
+		
+		String view = "events";
+		model.addAttribute("events", getAllEvents());
+	    return new ModelAndView(view, "command", model);
+		
+	}
+	
+	@PostMapping("/events")
+	public View getQuestionsToEvent(Model model, @ModelAttribute("event") Event event) {
+		System.out.println(event.getId());
+		List<Question> allQuestionsToEvent = getQuestionsToEventPrivate(event);
+		model.addAttribute("questions", allQuestionsToEvent);
+		return new RedirectView("/questions");
+	}
+	
+	private List<Question> getQuestionsToEventPrivate(Event event) {
+		return event.getQuestions();
 	}
 }
--- thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/controller/QuestionsController.java
+++ thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/controller/QuestionsController.java
@@ -1,11 +1,13 @@
 package com.badenia.feedback.thymeleaf.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.badenia.feedback.thymeleaf.model.Event;
 import com.badenia.feedback.thymeleaf.model.Question;
@@ -18,9 +20,9 @@ public class QuestionsController {
 IFeedbackClientService serviceReposiroty = new FeedbackClientService();
 	
 	@GetMapping("/questions")
-	public String index(Model model) {
+	public String index(Model model, @RequestParam(defaultValue="1") int idEvent) {
 		List<Event> allEvents = serviceReposiroty.leseAlleEvents();
-		List<Question> allQuestionsToEvent = getQuestionsToEvent(allEvents, 1);
+		List<Question> allQuestionsToEvent = getQuestionsToEvent(allEvents, idEvent);
 		model.addAttribute("questions", allQuestionsToEvent);
 		return "questions";
 	}
