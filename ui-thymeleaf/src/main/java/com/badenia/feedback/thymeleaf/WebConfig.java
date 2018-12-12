cceee7dd59b783e72becf0a92b5e10c1e53882f0
Constantin Krüger <Constantinidis@web.de>
2018-12-12 16:49:56 +0100

    change from yaml config to properties file

--- ui-thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/WebConfig.java
+++ ui-thymeleaf/src/main/java/com/badenia/feedback/thymeleaf/WebConfig.java
@@ -3,11 +3,12 @@ package com.badenia.feedback.thymeleaf;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
